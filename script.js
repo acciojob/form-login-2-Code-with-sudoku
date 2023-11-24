3e890d0f38d2bedcfd5b47843d9a68ffa17cefcf
Code-with-sudoku <137153314+Code-with-sudoku@users.noreply.github.com>
2023-11-24 11:59:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 //your JS code here. If required.
-<form>
-	<label for="First Name">First Name:</label>
-	<input type="text" name="First Name" id="fname"/><br/>
-	<label for="Last Name">Last Name:</label>
-	<input type="text" name="Last Name" id="lname"/><br/>
-	<label for="Phone Number">Phone Number:</label>
-	<input type="text" name="Phone Number" id="pnumber"/><br/>
-	<label for="Email ID">Email ID:</label>
-	<input type="text" name="Email ID" id="emailId"/> <br/>
-
-	<button type="submit" class="button">Submit</button>
-
-
-	</form>const fname = document.querySelector("#fname");
+const fname = document.querySelector("#fname");
 const lname = document.querySelector("#lname");
 const pnumber = document.querySelector("#pnumber");
 const email  =document.querySelector("#emailId");
